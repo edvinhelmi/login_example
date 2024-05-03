0e771f4784385cd93596495f2115370e506798f0
edvinhelmi <edvinhelmi@gmail.com>
2024-05-03 08:50:32 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="content">
+        <form action="data_search.php" method="POST">
+            <div class="container">
+                <div class="jst-center align flex c">
+                    <p class="font fw700 title">SITO GABBO</p>
+                </div>
+                <div>
+                    <input class="font fw500 textbox " type="text " name="username" placeholder="Email address">
+                </div>
+                <div>
+                    <input class="font fw500 textbox" type="password" name="psw" placeholder="Password">
+                </div>
+                <div class="flex jst-center align">
+                    <input class="button font fw500" type="submit" value="Accedi">
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
